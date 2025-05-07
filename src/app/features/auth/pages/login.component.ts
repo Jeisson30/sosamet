f9f3edac2cf3d719fa25e577a4434cbf63c38338
Jeisson <jeda1989@gmail.com>
2025-05-07 00:04:53 -0500

    Feat: Se realiza maquetado login y actualización rama main

--- src/app/features/auth/pages/login.component.ts
+++ src/app/features/auth/pages/login.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
-import { FloatLabelModule } from 'primeng/floatlabel';
+import { CheckboxModule } from 'primeng/checkbox';
+import { RippleModule } from 'primeng/ripple';
+import { CommonModule } from '@angular/common';
+import { FloatLabelModule }from 'primeng/floatlabel'
 
 @Component({
   selector: 'app-login',
@@ -10,18 +13,23 @@ import { FloatLabelModule } from 'primeng/floatlabel';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
   imports: [
+    CommonModule,
     FormsModule,
     InputTextModule,
     ButtonModule,
+    CheckboxModule,
+    RippleModule,
     FloatLabelModule
   ]
 })
 export class LoginComponent {
   username: string = '';
   password: string = '';
+  rememberMe: boolean = false;
 
   login() {
     console.log('Usuario:', this.username);
     console.log('Contraseña:', this.password);
+    console.log('Recuérdame:', this.rememberMe);    
   }
 }
